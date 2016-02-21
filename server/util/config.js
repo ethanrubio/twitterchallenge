d62365157ea0a2f85825c0b85121ed8b829f1400
Ethan Rubio <ethanrubio@gmail.com>
2016-02-20 19:13:31 -0800

    ready for deployment again

---
+++ server/util/config.js
@@ -0,0 +1,13 @@
+// api keys
+// twitter API
+
+var keys = {};
+
+keys.twitter = {
+  consumer_key: 'g5zhw7T0dOuZbsV6NW7czGYWV',
+  consumer_secret: 'dP1rfLo5CZD96FucUc8ixfcFcCWFJYbZ51YAwLCmmhvaTTqvfy',
+  token: '606060863-0otnfGsfEYgVHPEX1b10PHkpCWFkSiXhpjBRbZ1Z',
+  token_secret: 'H44MD1zjkkhN8vZONsLMVQcTBHGdyD9v7IafJ21tytb79'
+};
+
+module.exports = keys;
